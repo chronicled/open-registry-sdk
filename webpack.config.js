2b2f7369dca955b4411b20e18901390c1787cc9a
Orlando Castillo <orlandoc1618@gmail.com>
2016-08-18 18:23:35 -0700

    webpack build step

--- webpack.config.js
+++ webpack.config.js
@@ -14,36 +14,41 @@
 
 var path = require('path');
 var webpack = require('webpack');
+var libraryName = 'open-registry-sdk';
+var outputFile = libraryName + '.js';
 
 module.exports = {
-    entry: path.join(__dirname, 'index.js'),
-    output: {
-        path: path.join(__dirname, 'browser'),
-        filename: "bundle.min.js",
-        library: "OpenRegistrySDK",
-        libraryTarget: "var"
-    },
-    module: {
-        preLoaders: [
-            { test: /\.json/, loader: "json-loader" }
-        ],
-        loaders: [
-            {
-                test: /\.js/,
-                exclude: /node_modules\/(?!(ethereumjs-tx|web3-provider-engine|es6-promises)\/).*/,
-                loader: "babel",
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    node: {
-      fs: "empty"
-    },
-    plugins: [
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    ]
+  entry: path.join(__dirname, 'index.js'),
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    preLoaders: [
+      {test: /\.json/, loader: "json-loader"}
+    ],
+    noParse: /node_modules\/json-schema\/lib\/validate\.js/,
+    loaders: [{
+      test: /\.js/,
+      loader: "babel",
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  node: {
+    console: true,
+    fs: "empty",
+    net: 'empty',
+    tls: 'emtpy'
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ]
 };
