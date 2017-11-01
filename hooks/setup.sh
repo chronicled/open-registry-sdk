0540820160bf5db54ee470c51a15fadc85535e7f
orlandoc01 <orlandoc1618@gmail.com>
2017-11-01 15:58:52 -0700

    fix hook

--- hooks/setup.sh
+++ hooks/setup.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 # Sets up git hooks
 
-REPO=`git rev-parse --show-toplevel`
-cd $REPO
+function git_is_installed {
+  command -v git >/dev/null 2>&1
+}
+function is_git_repo {
+  git_is_installed && git status >/dev/null 2>&1
+}
+function is_sdk_repo {
+  if is_git_repo; then
+    local REPO=`git rev-parse --show-toplevel`
+    cd $REPO
+    [ "$(basename $REPO)" == "open-registry-sdk" ]
+  else 
+    false
+  fi
+}
+function is_sdk_repo_with_no_hooks {
+  is_sdk_repo && [ ! -L .git/hooks/pre-commit ]
+}
 
-if [ ! -L .git/hooks/pre-commit ]; then
-  echo "Setting up pre-commit hook"
+if is_sdk_repo_with_no_hooks; then
+  echo "Setting up pre-commit hooks"
   ln -si ../../hooks/pre-commit.sh .git/hooks/pre-commit
 fi
